9156fe5007d32cf8403ca8a35e5db9b41e5ac5ea
Fabio Carturan <160728760+LeFebius@users.noreply.github.com>
2026-02-10 13:47:06 +0100

    Delete Laboratorio Algoritimi di Programmazione/TecnologieWeb/Tecnologie_Web directory

--- Laboratorio Algoritimi di Programmazione/TecnologieWeb/Tecnologie_Web/client/src/pages/ReviewsPage.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReviewsSection from '../components/features/ReviewsSection';
-
-const ReviewsPage: React.FC = () => {
-    return <ReviewsSection />;
-};
-
-export default ReviewsPage;
